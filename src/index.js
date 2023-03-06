2bf0cf305a23eda0f9edb5b261b7d5fc9df63c6b
guilhermefaj <figueiredoguiu@gmail.com>
2023-03-06 10:16:06 -0300

    Refactor: reset.css como GlobalStyle

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './styles/reset.css';
 import App from './App';
 import GlobalStyle from './GlobalStyle';
 import styled from 'styled-components';
